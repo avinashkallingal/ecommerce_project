381375c52f1e48e9b667c0cfc6eb7b6651794b50
Avinash Kumar A <90566941+avinashkallingal@users.noreply.github.com>
2024-02-29 10:30:26 +0530

    graph data sorted

--- controlers/salesReportControler.js
+++ controlers/salesReportControler.js
@@ -27,6 +27,8 @@ const garphDataFetchDaily = async (req, res) => {
                 orderDate: "$_id",
                 productCount: 1
             }
+        },{
+            $sort:{date:1}
         }
     ]);
     console.log(data + " this is back end graph daily fetch data")
